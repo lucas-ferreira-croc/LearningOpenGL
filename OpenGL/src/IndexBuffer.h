ca05625bf918fa119a4277dfbcf49c807cacee42
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-05-06 16:01:41 -0300

    Abstracting VertexBuffer, IndexBuffer and ErrorHandling

---
+++ OpenGL/src/IndexBuffer.h
@@ -0,0 +1,16 @@
+
+class IndexBuffer 
+{
+private:
+	unsigned int m_RendererID;
+	unsigned int m_Count;
+
+	unsigned int GetCount() const { return m_Count; }
+
+public:
+	IndexBuffer(const unsigned int* data, unsigned int count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+};
---
+++ OpenGL/src/Renderer.h
@@ -0,0 +1,24 @@
+#include <GL/glew.h>
+
+#ifdef DEBUG
+    #define ASSERT(x) if (!(x)) __debugbreak();
+    #define GLCall(x) GLClearError();\
+        x;\
+        ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+//assertLogCall(__FUNCTION__, __FILE__, __LINE__)    
+
+/*
+inline static void assertLogCall(const char* function, const char* file, int line)
+{
+    assert(GLLogCall(function, file, line));
+}
+*/
+#else
+    #define GLCall(x)x
+#endif
+
+void GLClearError();
+bool GLLogCall(const char* function, const char* file, int line);
+
+
