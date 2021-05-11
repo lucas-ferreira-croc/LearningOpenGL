dff8999dd63f64227571e137990e28713f55e43c
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-05-11 16:11:25 -0300

    Creating menu

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -114,7 +114,12 @@ int main(void)
 
         ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
 
-        test::TestClearColor test;
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
+
+        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+
 
         /*glm::vec3 translationA(200, 200, 0);
         glm::vec3 translationB(400, 200, 0);
@@ -124,15 +129,27 @@ int main(void)
         float incrementR = 0.5f;*/
         while (!glfwWindowShouldClose(window))
         {
+            GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+
             renderer.Clear();
 
-            test.OnUpdate(0.0f);
-            test.OnRender();
 
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
-            test.OnImGuiRender();
+            if (currentTest)
+            {
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
+            }   
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
             /*
@@ -177,12 +194,16 @@ int main(void)
 
         }
 
-        ImGui_ImplOpenGL3_Shutdown();
-        ImGui_ImplGlfw_Shutdown();
-        ImGui::DestroyContext();
+        delete currentTest;
+        if (currentTest != testMenu)
+            delete testMenu;
 
-        glfwTerminate();
-        return 0;
     }
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+    return 0;
 }
--- OpenGL/src/tests/Test.h
+++ OpenGL/src/tests/Test.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <functional>
+#include <vector>
+#include <iostream>
 
 namespace test {
 
@@ -13,4 +16,23 @@ namespace test {
 		virtual void OnImGuiRender() {}
 	};
 
+	class TestMenu : public Test 
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+		
+		void OnImGuiRender() override;
+		
+		template<typename T>
+		void RegisterTest(const std::string& name) 
+		{
+			std::cout << "Registering test " << name << std::endl;
+			
+			m_Tests.push_back(std::make_pair(name, []() { return new T(); }));
+		}
+	
+	private:
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::function<Test* ()>>> m_Tests;
+	};
 }
