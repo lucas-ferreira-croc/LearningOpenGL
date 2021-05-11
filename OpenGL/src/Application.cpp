8a3780195f43703fde1ae821e95a79c81f7cb7be
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-05-11 17:11:32 -0300

    Texture testing implemented

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -20,6 +20,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void)
 {
@@ -50,59 +51,9 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
-        /*
-        
-        float positions[]{
-            //100.0f, 100.0f, 0.0f, 0.0f,  //0
-           -100.0f,  -100.0f, 0.0f, 0.0f,  //0
-            100.0f,  -100.0f, 1.0f, 0.0f,  //1
-            100.0f,   100.0f, 1.0f, 1.0f,  //2
-           -100.0f,   100.0f, 0.0f, 1.0f   //3
-
-        };
-
-        unsigned int indices[]{
-            0, 1, 2,
-            2, 3, 0
-        };
-
-        */
 
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-        //unsigned int  vao;
-        //GLCall(glGenVertexArrays(1, &vao));
-        //GLCall(glBindVertexArray(vao));
-
-        /*VertexArray va;
-
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-        Shader shader("res/shaders/Basic.shader");
-
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.4f, 0.8f, 1.0f);
-
-        Texture texture("res/textures/CrocEscrito.png");
-
-        texture.Bind();
-        shader.setUniform1i("u_Texture", 0);
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();*/
 
         Renderer renderer;
 
@@ -119,14 +70,8 @@ int main(void)
         currentTest = testMenu;
 
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
-
-        /*glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
-
-
-        float r = 0.0f;
-        float incrementR = 0.5f;*/
         while (!glfwWindowShouldClose(window))
         {
             GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
@@ -152,43 +97,7 @@ int main(void)
             }   
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-            /*
-              shader.Bind();
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMat4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader);
-            }
-
-            //shader.SetUniform4f("u_Color", r, 0.4f, 0.8f, 1.0f);
-
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.SetUniformMat4f("u_MVP", mvp);
-                renderer.Draw(va, ib, shader);
-            }
-
-
-            if (r > 1.0f)
-                incrementR = -0.05f;
-            else if (r < 0.0f)
-                incrementR = 0.05f;
-
-            r += incrementR;
-
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
-
-            */
-           
-
-
+            
             GLCall(glfwSwapBuffers(window));
             GLCall(glfwPollEvents());
 
